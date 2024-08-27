f41ce0b1055334ebc4a51496d0a6abd615ac40d7
Joonsoo <137869562+Joonsoooo@users.noreply.github.com>
2024-08-27 16:56:19 +0900

    WinAPI를 사용하여 윈도우 애플리케이션을 생성하고 화면에 출력 (주석없음)

---
+++ 과제들/WinAPI/WinMain.cpp
@@ -0,0 +1,87 @@
+#include <Windows.h>
+#include <tchar.h>
+
+HINSTANCE _hInstance;
+HWND _hWnd;
+LPTSTR _lpszClass = TEXT("Windows API");
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+    _hInstance = hInstance;
+
+    WNDCLASS wndclass;
+
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hInstance = hInstance;
+    wndclass.lpfnWndProc = (WNDPROC)WndProc;
+    wndclass.lpszClassName = _lpszClass;
+    wndclass.lpszMenuName = NULL;
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+
+    RegisterClass(&wndclass);
+
+    _hWnd = CreateWindowW(
+        _lpszClass,
+        _lpszClass,
+        WS_OVERLAPPEDWINDOW,
+        400,
+        100,
+        800,
+        800,
+        NULL,
+        (HMENU)NULL,
+        hInstance,
+        NULL
+    );
+
+    ShowWindow(_hWnd, nCmdShow);
+
+    MSG message;
+    while (GetMessage(&message, 0, 0, 0))
+    {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+
+    return message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+    switch (iMessage)
+    {
+    case WM_CREATE:
+        break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
